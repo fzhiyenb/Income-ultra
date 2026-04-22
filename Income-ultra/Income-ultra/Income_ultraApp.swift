7f4d12b89d0d8a2b9008260cca63993bc42a1ac8
fariza.zhiyenbayeva <fariza.zhiyenbayeva@ottonova.de>
2026-04-22 17:53:24 +0200

    project setup

---
+++ Income-ultra/Income-ultra/Income_ultraApp.swift
@@ -0,0 +1,17 @@
+//
+//  Income_ultraApp.swift
+//  Income-ultra
+//
+//  Created by Fariza Zhiyenbayeva on 22.04.26.
+//
+
+import SwiftUI
+
+@main
+struct Income_ultraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppStartingView()
+        }
+    }
+}
