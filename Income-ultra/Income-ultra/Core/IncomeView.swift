b0ad4d513fe70493761acac629019d56e9c284fe
fariza.zhiyenbayeva <fariza.zhiyenbayeva@ottonova.de>
2026-04-23 17:10:52 +0200

    HomeTabView

---
+++ Income-ultra/Income-ultra/Core/IncomeView.swift
@@ -0,0 +1,18 @@
+//
+//  IncomeView.swift
+//  Income-ultra
+//
+//  Created by Fariza Zhiyenbayeva on 23.04.26.
+//
+
+import SwiftUI
+
+struct IncomeView: View {
+    var body: some View {
+        Text("Income View!")
+    }
+}
+
+#Preview {
+    IncomeView()
+}
---
+++ Income-ultra/Income-ultra/Core/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Income-ultra
+//
+//  Created by Fariza Zhiyenbayeva on 23.04.26.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
